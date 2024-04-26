a92d3e520b94d0b4d29f89338558320a56ae32f2
MarioImmel <mario@immel-eich.de>
2024-04-26 08:31:53 +0100

    134 current  2024-04-25 17:07:13  24.05.20240416.66adc1e  6.6.0-asahi                          *

--- modules/gaming-mac.nix
+++ modules/gaming-mac.nix
@@ -9,9 +9,28 @@
     inputs.home-manager.nixosModules.default
   ];
 
+  /*# use x86_64 steam and allow unfree license
+  nixpkgs.overlays = [(self: super: let
+    x86pkgs = import pkgs.path { system = "x86_64-linux";
+      config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
+        "steam"
+        "steam-original"
+        "steam-runtime"
+        "steam-run"
+      ];
+    };
+  in {
+    inherit (x86pkgs) steam steam-run;
+  })];
+
+  # allow build for x86_64-linux architecture through emulation
+  boot.binfmt.emulatedSystems = [ "x86_64-linux" ];*/
+
   # packages
   environment.systemPackages = with pkgs; [
     box64
+    #steam
+    #steam-run
   ];
 }
 
